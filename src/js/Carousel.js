8f5ee6d8f251dfee79386dd53de43a098c125840
Ivan Mattie <mattieivan@gmail.com>
2017-06-01 17:57:34 -0600

    Made super basic next() and previous() methods

--- src/js/Carousel.js
+++ src/js/Carousel.js
@@ -2,6 +2,11 @@ export class Carousel {
 
     constructor(element) {
         this.element = element;
+        this.tray = this.element.querySelector(".tray");
+        this.children = this.tray.children;
+        this.movementIncrement = 100;
+        this.movementUnit = "%";
+        this.currentMovement = 0;
 
         this.upgrade();
     }
@@ -12,13 +17,35 @@ export class Carousel {
     upgrade() {
         this.element.classList.add("_simple-carousel");
 
-        var x, child, children = this.element.children;
+        var x, child, children = this.children;
 
         for(x = 0; x < children.length; x++) {
             child = children[x];
 
             child.classList.add("_carousel-element");
         }
+    }
+
+    next() {
+        var amountToMove = this.currentMovement + this.movementIncrement;
+
+        this.moveTo(amountToMove);
+    }
+
+    previous() {
+        var amountToMove = this.currentMovement - this.movementIncrement;
+
+        this.moveTo(amountToMove);
+    }
+
+    moveTo(locationIncrement) {
+        var computedMove = locationIncrement + this.movementUnit;
+
+        requestAnimationFrame(function() {
+            this.tray.style.transform = "translateX(-" + computedMove + ")";
+        }.bind(this));
+
+        this.currentMovement = locationIncrement;
     }
 
 }
