93e2ad18b78980914ec5a54a9e51ad07885c2559
Ivan Mattie <mattieivan@gmail.com>
2017-06-01 21:09:52 -0600

    Basic working model of moving next and backwards without going out of bounds

--- src/js/Carousel.js
+++ src/js/Carousel.js
@@ -7,8 +7,12 @@ export class Carousel {
         this.movementIncrement = 100;
         this.movementUnit = "%";
         this.currentMovement = 0;
+        this.isAgainstLeftWall = false;
+        this.isAgainstRightWall = false;
+        this.isMoving = false;
 
         this.upgrade();
+        this.computeStatus();
     }
 
     /**
@@ -24,28 +28,91 @@ export class Carousel {
 
             child.classList.add("_carousel-element");
         }
+
+        this.tray.addEventListener("transitionend", function(e) {
+            this.movementEnded(e);
+        }.bind(this), false);
+    }
+
+    movementEnded(e) {
+        this.isMoving = false;
+
+        this.computeStatus();
     }
 
     next() {
+        if(this.isAgainstRightWall) {
+            return;
+        }
+
         var amountToMove = this.currentMovement + this.movementIncrement;
 
         this.moveTo(amountToMove);
     }
 
     previous() {
+        if(this.isAgainstLeftWall) {
+            return;
+        }
+
         var amountToMove = this.currentMovement - this.movementIncrement;
 
         this.moveTo(amountToMove);
     }
 
     moveTo(locationIncrement) {
-        var computedMove = locationIncrement + this.movementUnit;
+        if(this.isMoving) {
+            return;
+        }
+
+        var computedMove = (-1 * locationIncrement) + this.movementUnit;
 
         requestAnimationFrame(function() {
-            this.tray.style.transform = "translateX(-" + computedMove + ")";
+            this.tray.style.transform = "translateX(" + computedMove + ")";
         }.bind(this));
 
+        this.isMoving = true;
         this.currentMovement = locationIncrement;
+    }
+
+    getChildWidthSum() {
+        var x, sum = 0, child;
+
+        for(x = 0; x < this.children.length; x++) {
+            child = this.children[x];
+
+            sum += child.offsetWidth;
+        }
+
+        return sum;
+    }
+
+    getCurrentMovementInPixels() {
+        if(this.movementUnit !== '%') {
+            return this.currentMovement;
+        }
+
+        var parentWidth = this.element.offsetWidth;
+        var pixelMovementValue = parentWidth * (this.currentMovement * 0.01);
+
+        return pixelMovementValue;
+    }
+
+    computeStatus() {
+        var sum = this.getChildWidthSum();
+        var movementInPixels = this.getCurrentMovementInPixels();
+
+        if(movementInPixels == 0) {
+            this.isAgainstLeftWall = true;
+        } else {
+            this.isAgainstLeftWall = false;
+        }
+
+        if(movementInPixels + this.element.offsetWidth > sum) {
+            this.isAgainstRightWall = true;
+        } else {
+            this.isAgainstRightWall = false;
+        }
     }
 
 }
